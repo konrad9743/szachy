ed3eb2a85d19a2a08f15252820cbf1dd939ff3c8
konrad9743 <116022112+konrad9743@users.noreply.github.com>
2023-11-07 14:46:37 +0100

    Add files via upload

---
+++ Magnus_verify.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chessMoveForm = document.getElementById("chessMoveForm");
+    const resultMessage = document.getElementById("resultMessage");
+
+    chessMoveForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const bestMove = document.getElementById("bestMove").value.trim();
+
+        if (validateChessMove(bestMove)) {
+            resultMessage.textContent = "Dobry Ruch!";
+        } else {
+            resultMessage.textContent = "Ruch nie jest poprawny :(";
+        }
+    });
+
+    function validateChessMove(bestMove) {
+
+        return bestMove.toLowerCase() === "qa7";
+    }
+});
